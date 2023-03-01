3346bad9dbdab69065c409bf98a6820c7c09e4a4
jrolls81 <105545633+jrolls81@users.noreply.github.com>
2023-03-01 10:32:06 -0600

    re-did task three. I didn't realize step four was adding the display code, so I had the controller returning to the list-jobs, instead of the search template. Fixed that had passing tests. Then completed task four with passing tests

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -42,6 +42,6 @@ public class SearchController {
         model.addAttribute("jobs", jobs);
         model.addAttribute("columns", columnChoices);
 
-        return "list-jobs";
+        return "search";
     }
 }
